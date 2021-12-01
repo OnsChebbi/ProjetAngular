092c885c419b935364e58ad7b7bea5d58c6bb449
jesserdhieb1 <dhieb.jesser@esprit.tn>
2021-12-01 23:30:18 +0100

    landing-page component + some changes

---
+++ src/app/landing-page/landing-page-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {MainProviderComponent} from "../app-provider/main-provider/main-provider.component";
+import {HomePageComponent} from "./home-page/home-page.component";
+
+const routes: Routes = [
+  {path: 'home',component: HomePageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class LandingPageRoutingModule { }

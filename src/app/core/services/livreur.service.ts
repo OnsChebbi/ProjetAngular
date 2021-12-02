388aa193e7a298b0f2c8559abc3d5a779fe4311b
OnsChebbi <ons.chebbi@esprit.tn>
2021-12-02 17:17:40 +0100

    Fixation du template

--- src/app/core/services/livreur.service.ts
+++ src/app/core/services/livreur.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Livreur } from '../model/livreur';
+import { environment } from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LivreurService {
 
-  constructor() { }
+  
+  url=environment.url+"livreur/";
+  constructor(private http: HttpClient) { }
+  currentLivreur:Livreur;
+  status:boolean=false;
+  getListLivreurService(){
+    return this.http.get<Livreur[]>(this.url)
+    
+    }
+    addListService(livreur: Livreur){
+      return this.http.post(this.url+"add-livreur/", livreur)
+    }
+    deleteListService(id:number){
+      return this.http.delete(this.url+id)
+    }
+   
+    updateListService(livreur: Livreur){
+      return this.http.put(this.url+livreur.idLiv,livreur)
+    }
+
+    getLivreurServiceById(id:string){
+     
+    }
 }
+
+

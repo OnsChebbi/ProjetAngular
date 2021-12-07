bc13f1ccd94b515ac1593aa09ef1824ba6bed420
jesser dhieb <73405867+jesserdhieb1@users.noreply.github.com>
2021-12-07 22:45:00 +0100

    child parent

--- src/app/user/show-all/show-all.component.ts
+++ src/app/user/show-all/show-all.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {UserService} from "../../core/services/user.service";
 import {User} from "../../core/model/user";
 import {Router} from "@angular/router";
--- src/app/user/show/show.component.ts
+++ src/app/user/show/show.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from "../../core/model/user";
 
 @Component({
   selector: 'app-show',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowComponent implements OnInit {
 
+@Input()listOfUsers:User[];
   constructor() { }
 
   ngOnInit(): void {

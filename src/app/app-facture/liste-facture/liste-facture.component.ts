a6f904349539b9dcac9bf4973a3b01d4f57c05d6
AdamElamri <pearlblack257@gmail.com>
2021-12-08 14:11:33 +0100

    aaa

---
+++ src/app/app-facture/liste-facture/liste-facture.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-facture',
+  templateUrl: './liste-facture.component.html',
+  styleUrls: ['./liste-facture.component.css']
+})
+export class ListeFactureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -7,8 +7,6 @@ import { ShowAllComponent } from './show-all/show-all.component';
 import {BrowserModule} from "@angular/platform-browser";
 import { ShowOneComponent } from './show-one/show-one.component';
 
-
-
 @NgModule({
   declarations: [
     AddFormComponent,

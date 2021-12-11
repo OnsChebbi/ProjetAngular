ffdaf740b8e8c4e09d5bcc3706d1ee4c30c88c5b
OumaymaFarhani <73642477+OumaymaFarhani@users.noreply.github.com>
2021-12-11 03:41:23 +0100

    oumaymaproduit

--- src/app/core/services/produit.service.ts
+++ src/app/core/services/produit.service.ts
@@ -3,6 +3,8 @@ import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Produit} from "../model/produit";
 import {User} from "../model/user";
+import {Provider} from "../model/provider";
+import {Stock} from "../model/stock";
 
 @Injectable({
   providedIn: 'root'
@@ -19,28 +21,31 @@ export class ProduitService {
     this.currentProduct=produit;
     this.status=true;
   }
-  url=environment.url+"product/";
+  url=environment.url+"produit/";
   constructor(private http:HttpClient) { }
   getListProductService(){
     return this.http.get<Produit[]>(this.url+"retrieve-all-produits");
   }
   addProductService(product:Produit){
-    return this.http.post(this.url+"addProduit",product);
+    return this.http.post(this.url+"addProduit/",product);
+
   }
 
 
-
-
   deleteProductService(id:number){
-    return this.http.delete(this.url+id);
+    return this.http.delete(this.url+'remove-produit/'+id);
   }
   getProductServiceById(id:any){
     return this.http.get<Produit>(this.url+id);
   }
   updateProductService(id:number,product:Produit){
-    return this.http.put(this.url+"/"+id,product);
+    return this.http.put(this.url+"modifier_produit/"+id,product);
   }
-//added by adem 
+
+
+
+
+//added by adem
   getSingleProduct(id:number){
     return this.http.get(`${this.url}getProduit/${id}`);
   }

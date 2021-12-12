607b2fa950c5fdacd4514cb53bdc4a6b62d28de7
jesser dhieb <73405867+jesserdhieb1@users.noreply.github.com>
2021-12-12 21:32:46 +0100

    modify user verif email

--- src/app/user/update-form/update-form.component.ts
+++ src/app/user/update-form/update-form.component.ts
@@ -12,6 +12,8 @@ import {UserRole} from "../../core/model/UserRole";
 export class UpdateFormComponent implements OnInit {
   user: User=new User();
   //modelUSer:UserRole=new UserRole();
+  ancien_email:string="";
+  test_email:string="";
   constructor(private userService : UserService,private router: Router,private activated:ActivatedRoute) {  }
 
   ngOnInit(): void {
@@ -20,7 +22,8 @@ export class UpdateFormComponent implements OnInit {
         (params)=> {
           let id = params.get('id');
           this.userService.getUserServiceById(id).subscribe(
-            (data) => this.user = data
+            (data) => {this.user = data;
+                   this.ancien_email=this.user.email;}
           )
         }
       )
@@ -31,18 +34,17 @@ export class UpdateFormComponent implements OnInit {
       )
     }
 
-      /*this.user.categorieUser='Fidele';
-      this.user.role='Super_Admin';
-      this.user.facture=[];
-      this.user.avisUser=[];
-      this.userService.addUserService(this.user).subscribe(
-        ()=>this.router.navigate(['/show-all-user'])
-      )*/
-
-  return(){
-    this.router.navigate(['/show-all-user']);
-  }
-
-
+  TestEmail(email:string){
+    if (this.ancien_email!=email){
+    this.userService.VerifyEmail(email).subscribe(
+      (data:boolean)=>{
+        if (data==true){
+          this.test_email="existe";
+        }else {
+          this.test_email="existe pas";
+        }
+      }
+    )
+  }}
 
 }

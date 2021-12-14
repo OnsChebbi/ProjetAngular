31870a8a31be7e298f774c31a282f6c2bcc3c4ac
OumaymaFarhani <73642477+OumaymaFarhani@users.noreply.github.com>
2021-12-14 01:56:21 +0100

    up

--- src/app/app-product/main-product/main-product.component.ts
+++ src/app/app-product/main-product/main-product.component.ts
@@ -11,6 +11,8 @@ import {Router} from "@angular/router";
 export class MainProductComponent implements OnInit {
 
   listProduct: Produit[];
+  p:Produit;
+  liked:boolean=false;
   showFormTemplate: boolean;
   buttonValue: string;
   inputProduct: Produit // the parent component will send this input to the child (formProduct)
@@ -18,21 +20,38 @@ export class MainProductComponent implements OnInit {
 
   ngOnInit(): void {
     this.showFormTemplate = false;
-    this.buttonValue="add new Product";
+    this.buttonValue = "add new Product";
 
-   /* this.productService.getListProductService().subscribe(
-      (data:Produit[])=>this.listProduct=data
-    )*/
+    /* this.productService.getListProductService().subscribe(
+       (data:Produit[])=>this.listProduct=data
+     )*/
     this.productService.getListProductService().subscribe(
-      (data:Produit[])=>this.listProduct=data
+      (data: Produit[]) => this.listProduct = data
     )
+
+
+  }
+  like(product: Produit): void{
+
+    /*let i = this.listProduct.indexOf(product);
+    this.listProduct[i].nbrLike++
+    *///console.log(this.listProduct)
+    let i = this.listProduct.indexOf(product);
+    this.productService.getProductServiceLiked(this.listProduct[i].id).subscribe(
+      (data: Produit) => this.listProduct[i] = data
+    )
+    if(this.liked) {
+      this.liked = false;
+      this.listProduct[i].nbrLike--
+
+    }
+    else {
+      this.liked = true
+      this.listProduct[i].nbrLike++
+    }
+    console.log(this.liked)
   }
 
-  like(product: Produit): void{
-    let i = this.listProduct.indexOf(product);
-    this.listProduct[i].nbrLike++
-    //console.log(this.listProduct)
-  }
   //the method save will check if is an update case or adding a new product case
   // save(product: Produit): void{
   //   let i = this.listProduct.indexOf(product);
